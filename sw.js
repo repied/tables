22a5c2a082927c114373921cc306590c5799a6e4
repied <pierre.dangauthier@gmail.com>
2026-03-06 21:10:01 +0000

    Add missing checklists in cache

--- sw.js
+++ sw.js
@@ -11,6 +11,8 @@ const urlsToCache = [
     'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
     './assets/help_fr.md',
     './assets/help_en.md',
+    './assets/checklist_fr.md',
+    './assets/checklist_en.md',
     './src/qrcode.min.js',
     './src/jsQR.min.js',
     './data/mn90_stops.csv',
