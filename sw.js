0da0aaa449f57371aa4d1ee93e4e8a017e916f8e
repied <repied@users.noreply.github.com>
2026-04-17 11:27:43 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+
+    const clients = await self.clients.matchAll({ type: 'window' });
+    for (const client of clients) {
+        client.navigate('https://plongee.app/');
+    }
+
+    await self.registration.unregister();
+});
